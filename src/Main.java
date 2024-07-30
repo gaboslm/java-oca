860e804da9859176e5ac004fa7544334bad154c4
Gabriel Luna <gabriel.slmendoza@gmail.com>
2024-07-29 19:50:54 -0500

    string args from java explained

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        * Run following commands
+        * - javac Main.java
+        * - java Main John Doe
+        */
+        System.out.println("Execution arguments" + args[0] + args[1]);
+    }
+}
